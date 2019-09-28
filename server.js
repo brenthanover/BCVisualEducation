4e6bd1d849568c13977416f12e5686f6a23e1a15
Brent Hanover <brenthanover@gmail.com>
2019-09-28 14:28:05 -0700

    fix backend

--- server.js
+++ server.js
@@ -1,35 +1,81 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-const port = 5000;
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
 
-var indexRouter = require('./index');
-var reviewsRouter = require('./reviews');
-var testRouter = require('./test');
+// server variables
+var mongoose = require('mongoose');
+var indexRouter = require('./routes');
+var reviewsRouter = require('./routes/reviews');
+var testRouter = require('./routes/test');
+var connected = false;
+
+// start up mongo
+mongoose.Promise = global.Promise;
+var mongodb = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox-obiso.mongodb.net/assignment_4?retryWrites=true&w=majority';
+var db = mongoose.connect( mongodb || process.env.MONGODB_URI, (error) => {
+    if (error) {
+        console.log("error connecting to mongodb");
+        console.log(error);
+    } else {
+        console.log("successful connection to mongodb");
+        connected = true;
+    }
+});
 
 const server = express();
 
+// confirm connectivity to mongo
+server.get('/test_mongo', (req, res) => {
+    if (connected) {
+        res.send({express: 'YOUR MONGO DATABASE IS CONNECTED TO REACT'});
+    }
+});
+
 server.use(cors());
 
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'jade');
+// server.set('views', path.join(__dirname, 'views'));
+// server.set('view engine', 'jade');
 
 server.use(express.json());
 server.use(bodyParser.urlencoded({extended: false}));
-server.use(express.urlencoded({ extended: false }));
-server.use(express.static(path.join(__dirname, 'public')));
+// server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'client', 'build')));
 
 server.use('/', indexRouter);
 server.use('/reviews', reviewsRouter);
 server.use('/test', testRouter);
 
-// console.log that your server is up and running
-// server.listen(port, () => console.log('Listening on port ' + port));
+// if (process.env.NODE_ENV === 'production') {
+server.use(express.static(path.join(__dirname, 'client', 'build')));
 
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+});
+// }
+
+// console.log that your server is up and running
+server.listen(port, () => console.log('Listening on port ' + port));
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+server.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.status(500).json({
+        message: err.message,
+        error: err
+    });
+});
 
 module.exports = server;
-
-
-
